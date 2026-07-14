57abe9aebcc9f7868055aee802fd921627b0aed7
Diptendu16 <188218888+Diptendu16@users.noreply.github.com>
2026-07-14 22:40:22 +0530

    folder structure created along with the boiler plate code.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="p-8">Home Page</div>;
+};
+
+export default Home;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <div className="p-8">Register Page</div>;
+};
+
+export default Register;
